25fb5d55db3465a8b3cd9190bc6df83cd025221d
MAllen07 <mrsmallen07@gmail.com>
2019-06-06 18:24:10 -0400

    adjusted migration table information

--- migrations/20190606153921_add_cohorts_table.js
+++ migrations/20190606153921_add_cohorts_table.js
@@ -1,23 +1,15 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable("cohorts", function(tbl) {
-    tbl.increments();
-
-    // a varchar called name, 128, unique, not null
-    tbl
-      .integer("role_id")
-      .string("name", 128)
-      .notNullable()
-      .unique();
-
-    // Syntax for a foreign key
-    tbl;
-    // .integer("role_id")
-    // .unsigned()
-    // .references("id")
-    // .inTable("roles")
-    // .onDelete("CASCADE")
-    // .onUpdate("CASCADE");
-  });
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable("cohorts", function (tbl) {
+        //id
+        tbl.increments();
+        //name
+        tbl
+            .string("name", 128)
+            .notNullable()
+            .unique();
+    });
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists("cohorts");
+};
--- migrations/20190606155604_add_students_table.js
+++ migrations/20190606155604_add_students_table.js
@@ -1,25 +1,22 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable("students", function(tbl) {
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("students", function (tbl) {
+    //id
     tbl.increments();
-
-    // a varchar called name, 128, unique, not null
+    //name
     tbl
-      .integer("role_id")
       .string("name", 128)
-      .integer("role_id")
       .notNullable()
-
       .unique();
-
-    // Syntax for a foreign key
-    // tbl
-    //   .integer("role_id")
-    //   .unsigned()
-    //   .references("id")
-    //   .inTable("roles")
-    //   .onDelete("CASCADE")
-    //   .onUpdate("CASCADE");
+    tbl
+      .integer("cohorts_id")
+      .unsigned()
+      .references("id")
+      .inTable("cohorts")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
   });
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists("students");
+};
