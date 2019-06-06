a705fecb977acf70c3e99e922d46af52d1eaf3f0
MAllen07 <mrsmallen07@gmail.com>
2019-06-06 16:42:46 -0400

    adjusted cohorts and student files with the exports.up information

--- migrations/20190606155604_add_students_table.js
+++ migrations/20190606155604_add_students_table.js
@@ -1,22 +1,23 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("cohorts", function(tbl) {
+  return knex.schema.createTable("students", function(tbl) {
     tbl.increments();
 
     // a varchar called name, 128, unique, not null
     tbl
       .integer("role_id")
       .string("name", 128)
+      .integer("role_id")
       .notNullable()
       .unique();
 
     // Syntax for a foreign key
-    tbl;
-    // .integer("role_id")
-    // .unsigned()
-    // .references("id")
-    // .inTable("roles")
-    // .onDelete("CASCADE")
-    // .onUpdate("CASCADE");
+    // tbl
+    //   .integer("role_id")
+    //   .unsigned()
+    //   .references("id")
+    //   .inTable("roles")
+    //   .onDelete("CASCADE")
+    //   .onUpdate("CASCADE");
   });
 };
 
